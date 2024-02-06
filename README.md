e72d1b5fda1705257b365d0e2a161fcdd18113eb
monkeyx-net <tim@monkeyx.net>
2024-02-06 12:29:58 +0000

    Initial comit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Welcome
+
+
+To build this version run make in src
+
+This is a basic template for use in portmaster
+
+makefile runs strip automatically to reduce file size.
+
+![image info](imgui controls v6 - Xbox.png)
+
+
+TODO
+- [x] Create template
+- [x] Build on aarch64
+- [x] Test on my RGB30 Jelos and ArkOS
+- [ ] Create CLI arguments for title and text
+- [ ] Create CLI arguments for buttons
+- [ ] Check full screen and scaling
---
+++ src/main.cpp
@@ -0,0 +1,199 @@
+// Dear ImGui: standalone example application for SDL2 + SDL_Renderer
+// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+
+// Learn about Dear ImGui:
+// - FAQ                  https://dearimgui.com/faq
+// - Getting Started      https://dearimgui.com/getting-started
+// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
+// - Introduction, links and more at the top of imgui.cpp
+
+// Important to understand: SDL_Renderer is an _optional_ component of SDL2.
+// For a multi-platform app consider using e.g. SDL+DirectX on Windows and SDL+OpenGL on Linux/OSX.
+
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_sdlrenderer2.h"
+#include <SDL.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+#if !SDL_VERSION_ATLEAST(2,0,17)
+#error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
+#endif
+
+
+std::string read_file(std::ifstream in_text )
+{
+    //std::ifstream in_text (in_text);
+    std::string line;
+    std::string all_lines;
+    if (in_text.is_open())
+    {
+        while ( getline (in_text,line) )
+        {
+            all_lines += line + "\n";
+        }
+    in_text.close();
+  }
+  return all_lines;
+}
+
+// Main code
+int main(int, char**)
+{
+    // Setup SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+    {
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // From 2.0.18: Enable native IME.
+#ifdef SDL_HINT_IME_SHOW_UI
+    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+#endif
+
+    // Create window with SDL_Renderer graphics context
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window* window = SDL_CreateWindow("Set IP Address for game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720, 720, window_flags);
+    if (window == nullptr)
+    {
+        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+        return -1;
+    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        SDL_Log("Error creating SDL_Renderer!");
+        return 0;
+    }
+    //SDL_RendererInfo info;
+    //SDL_GetRendererInfo(renderer, &info);
+    //SDL_Log("Current SDL_Renderer: %s", info.name);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
+
+    //Using open source Roboto Medium. Remark the line below to use defaul 13 point terminal font.
+    io.Fonts->AddFontFromFileTTF("Roboto-Medium.ttf", 24.0f);
+    
+    // Our state
+   
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
+    //Read Ip address and instructions before the starting the loop
+    std::string ip_load=read_file(std::ifstream("ip.txt"));
+    std::string instructions=read_file(std::ifstream("instructions.txt"));
+
+    // Vector for IP Address Input  ImGui::DragInt4
+    static int vec4i[] = { 172, 16, 8, 15 }; 
+
+    // Main loop
+    bool done = false;
+    while (!done)
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                done = true;
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+                done = true;
+        }
+        
+       
+        // Start the Dear ImGui frame
+        ImGui_ImplSDLRenderer2_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+
+        // 2. Show a simple window using a Begin/End pair to create a named window.
+        {
+            bool p_open = true;
+            // Flags for some reason having empty flags removes the collapse button, which is why I look at them!
+            //ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
+            ImGui::Begin("Portmaster Test",&p_open, window_flags);
+           // static char str0[128] = "Edit Text Test";
+           // ImGui::InputText("##Input", str0, IM_ARRAYSIZE(str0));
+            ImGui::Text("Currently set IP Address: %s",ip_load.c_str());
+            ImGui::Text("Server IP Adress :-");
+            ImGui::DragInt4("", vec4i, 1, 1, 255);
+            //ImGui::SetCursorPos(ImVec2(0,350));   // Place Button
+            if (ImGui::Button("Save IP Address",ImVec2(347,50)))
+            {
+                std::ostringstream stream;
+                for (size_t i = 0; i < sizeof(vec4i)/sizeof(vec4i[0]); ++i)
+                {
+                 if (i) stream << '.';
+                    stream << vec4i[i] ;
+                }
+                std::ofstream file("ip.txt");
+                std::string ip_string = stream.str();
+			    file << ip_string;
+                file.close();
+                ip_load = ip_string;
+                //printf("%d.%d.%d.%d", vec4i[0],vec4i[1],vec4i[2],vec4i[3]);
+            }
+            // Creates space
+            ImGui::Dummy(ImVec2(0.0f, 20.0f));
+            ImGui::TextWrapped("%s",instructions.c_str());
+            // Creates space
+            ImGui::Dummy(ImVec2(0.0f, 20.0f));
+            if (ImGui::Button("A Button",ImVec2(200,50)))
+            {
+                done = true;
+                //return 1;
+
+            }
+            ImGui::SameLine();
+             if (ImGui::Button("B Button",ImVec2(200,50)))
+            {
+                done = true;
+                return 121;        
+            }
+           
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+        SDL_SetRenderDrawColor(renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255), (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
+        SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+        SDL_RenderPresent(renderer);
+    }
+
+    // Cleanup
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
