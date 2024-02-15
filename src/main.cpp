6d852e06d9f26789bebdac05121b4124a2b87175
monkeyx-net <tim@monkeyx.net>
2024-02-15 17:36:45 +0000

    modified:   imgui.ini 	new file:   imgui_markdown.h 	modified:   instructions.txt 	modified:   main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 //IMGUI libraries to support SDL2 rendering
 #include "imgui.h"
+#include "imgui_markdown.h" 
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer2.h"
 #include <SDL.h>
@@ -18,6 +19,100 @@
 #error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
 #endif
 
+inline ImGui::MarkdownImageData ImageCallback( ImGui::MarkdownLinkCallbackData data_ );
+
+static ImFont* H1 = NULL;
+static ImFont* H2 = NULL;
+static ImFont* H3 = NULL;
+
+static ImGui::MarkdownConfig mdConfig;  
+
+inline ImGui::MarkdownImageData ImageCallback( ImGui::MarkdownLinkCallbackData data_ )
+{
+    // In your application you would load an image based on data_ input. Here we just use the imgui font texture.
+    ImTextureID image = ImGui::GetIO().Fonts->TexID;
+    // > C++14 can use ImGui::MarkdownImageData imageData{ true, false, image, ImVec2( 40.0f, 20.0f ) };
+    ImGui::MarkdownImageData imageData;
+    imageData.isValid =         true;
+    imageData.useLinkCallback = false;
+    imageData.user_texture_id = image;
+    imageData.size =            ImVec2( 40.0f, 20.0f );
+
+    // For image resize when available size.x > image width, add
+    ImVec2 const contentSize = ImGui::GetContentRegionAvail();
+    if( imageData.size.x > contentSize.x )
+    {
+        float const ratio = imageData.size.y/imageData.size.x;
+        imageData.size.x = contentSize.x;
+        imageData.size.y = contentSize.x*ratio;
+    }
+
+    return imageData;
+}
+
+
+void LoadFonts( float fontSize_ = 12.0f )
+{
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->Clear();
+    // Base font
+    io.Fonts->AddFontFromFileTTF( "Assets/Fonts/Roboto-Medium.ttf", fontSize_ );
+    // Bold headings H2 and H3
+    H2 = io.Fonts->AddFontFromFileTTF( "Assets/Fonts/Roboto-Medium.ttf", fontSize_ );
+    H3 = mdConfig.headingFormats[ 1 ].font;
+    // bold heading H1
+    float fontSizeH1 = fontSize_ * 1.1f;
+    H1 = io.Fonts->AddFontFromFileTTF( "Assets/Fonts/Roboto-Medium.ttf", fontSizeH1 );
+}
+
+void ExampleMarkdownFormatCallback( const ImGui::MarkdownFormatInfo& markdownFormatInfo_, bool start_ )
+{
+    // Call the default first so any settings can be overwritten by our implementation.
+    // Alternatively could be called or not called in a switch statement on a case by case basis.
+    // See defaultMarkdownFormatCallback definition for furhter examples of how to use it.
+    ImGui::defaultMarkdownFormatCallback( markdownFormatInfo_, start_ );        
+       
+    switch( markdownFormatInfo_.type )
+    {
+    // example: change the colour of heading level 2
+    case ImGui::MarkdownFormatType::HEADING:
+    {
+        if( markdownFormatInfo_.level == 2 )
+        {
+            if( start_ )
+            {
+                ImGui::PushStyleColor( ImGuiCol_Text, ImGui::GetStyle().Colors[ImGuiCol_TextDisabled] );
+            }
+            else
+            {
+                ImGui::PopStyleColor();
+            }
+        }
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+}
+
+
+void Markdown( const std::string& markdown_ )
+{
+    // You can make your own Markdown function with your prefered string container and markdown config.
+    // > C++14 can use ImGui::MarkdownConfig mdConfig{ LinkCallback, NULL, ImageCallback, ICON_FA_LINK, { { H1, true }, { H2, true }, { H3, false } }, NULL };
+    mdConfig.linkCallback =         NULL;
+    mdConfig.tooltipCallback =      NULL;
+    mdConfig.imageCallback =        ImageCallback;
+    mdConfig.linkIcon =             NULL;
+    mdConfig.headingFormats[0] =    { H1, true };
+    mdConfig.headingFormats[1] =    { H2, true };
+    mdConfig.headingFormats[2] =    { H3, false };
+    mdConfig.userData =             NULL;
+    mdConfig.formatCallback =       ExampleMarkdownFormatCallback;
+    ImGui::Markdown( markdown_.c_str(), markdown_.length(), mdConfig );
+}
 
 std::string read_file(std::ifstream in_text )
 {
@@ -220,6 +315,7 @@ int main(int argc, char *argv[])
     std::string instructions=read_file(std::ifstream("instructions.txt"));
     std::string readme=read_file(std::ifstream("README.md"));
     std::string licence=read_file(std::ifstream("imgui-demo_LICENSE.txt"));
+    
 
     std::string file_debug = exec("file imgui-demo");
     std::string ldd_debug = exec("ldd imgui-demo");
@@ -322,9 +418,10 @@ int main(int argc, char *argv[])
                         {
                             if (selected == 0)
                             {
-                                ImGui::TextWrapped("Navigaton Instructons: %s", instructions.c_str());
-                                ImGui::Separator();
-                                ImGui::TextWrapped("Game README.md: %s", readme.c_str());
+                                Markdown(instructions);
+                                Markdown( readme);
+                                //ImGui::TextWrapped("Game README.md: %s", readme.c_str());
+                                //ImGui::TextWrapped("Navigaton Instructons: %s", instructions.c_str());
                             }
                             if (selected == 1)
                             {
